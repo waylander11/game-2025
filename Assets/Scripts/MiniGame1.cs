a224749b0ce8be659671224b0559bda09b9f3775
Mr-Fikser <136245570+Mr-Fikser@users.noreply.github.com>
2025-02-15 15:33:03 +0200

    Перевірка GitHub
    Додав вступний текст в скрипт "MiniGame1"

---
+++ Assets/Scripts/MiniGame1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Хто читайє той ло... Лоус Дамбл-Бамбл😎"); 
+    }
+}
