2b514d8f66aa19b34c54e296058e75a40346084e
Mr-Fikser <136245570+Mr-Fikser@users.noreply.github.com>
2025-03-22 15:48:20 +0200

    виправлення багів в 3 в ряд

--- Assets/Scripts/3-in-rowScripts/PuzzleGenerator.cs
+++ Assets/Scripts/3-in-rowScripts/PuzzleGenerator.cs
@@ -8,9 +8,9 @@ public class PuzzleGenerator : MonoBehaviour
     public Texture[] elements;
     public int totalColumns = 9;
     public int totalRows = 9;
-    public UnityEngine.UI.Slider scoreSlider;
+    public Slider scoreSlider;
     private float sliderValue = 100f;
-
+    private bool isCheckingCombos = false;
 
     [System.Serializable]
     public class PuzzleElement
@@ -18,16 +18,24 @@ public class PuzzleGenerator : MonoBehaviour
         public Texture texture;
         public Vector2 position;
     }
-    private bool isCheckingCombos = false;
+    
+    private List<List<PuzzleElement>> columns = new List<List<PuzzleElement>>();
+    private int selectedColumn = -1, selectedRow = -1;
 
-    List<List<PuzzleElement>> columns = new List<List<PuzzleElement>>();
-
-    int selectedColumn = -1;
-    int selectedRow = -1;
-    int score;
     void Start()
     {
-        for(int x = 0; x < totalColumns; x++)
+        InitializeGrid();
+        StartCoroutine(RestockEnumrator());
+        StartCoroutine(DecreaseSliderOverTime());
+    }
+    void Update()
+    {
+    
+    }
+
+    void InitializeGrid()
+    {
+        for (int x = 0; x < totalColumns; x++)
         {
             List<PuzzleElement> column = new List<PuzzleElement>();
             for (int y = 0; y < totalRows; y++)
@@ -36,139 +44,97 @@ public class PuzzleGenerator : MonoBehaviour
             }
             columns.Add(column);
         }
-
-        StartCoroutine(RestockEnumrator());
-        StartCoroutine(DecreaseSliderOverTime());
     }
+
     IEnumerator DecreaseSliderOverTime()
     {
         while (true)
         {
-            yield return new WaitForSeconds(0.25f);
-            sliderValue -= 2f; 
+            yield return new WaitForSeconds(1f);
             sliderValue = Mathf.Clamp(sliderValue, 0, 100);
-            scoreSlider.value = sliderValue;
+            scoreSlider.value = sliderValue - 1f;
+            scoreSlider.value = Mathf.Lerp(scoreSlider.value, sliderValue, Time.deltaTime * 5);
         }
     }
 
-
     void OnGUI()
     {
-        GUIStyle style = new GUIStyle();
-        style.normal.textColor = Color.red;
-        GUI.Label((new Rect(5, 5, 400, 25)), "Score: " + score.ToString(), style);
-
         for (int x = 0; x < columns.Count; x++)
         {
             for (int y = 0; y < columns[x].Count; y++)
             {
-                if (columns[x][y].texture)
+                if (columns[x][y].texture != null)
                 {
-                    columns[x][y].position = Vector2.Lerp(columns[x][y].position, new Vector2((Screen.width / 2 - (columns.Count * 64) / 2) + x * 64, (Screen.height / 2 - (columns[x].Count * 64) / 2) + y * 64 + 30), Time.deltaTime * 7);
+                    Vector2 targetPos = new Vector2((Screen.width / 2 - (columns.Count * 64) / 2) + x * 64, (Screen.height / 2 - (columns[x].Count * 64) / 2) + y * 64 + 30);
+                    columns[x][y].position = Vector2.Lerp(columns[x][y].position, targetPos, Time.deltaTime * 7);
                     Rect elementRect = new Rect(columns[x][y].position.x, columns[x][y].position.y, 64, 64);
-                    if ((x == selectedColumn && (y == selectedRow - 1 || y == selectedRow + 1)) || ((x == selectedColumn - 1 || x == selectedColumn + 1) && y == selectedRow))
+
+                    if ((x == selectedColumn && (y == selectedRow - 1 || y == selectedRow + 1)) ||
+                        ((x == selectedColumn - 1 || x == selectedColumn + 1) && y == selectedRow))
                     {
                         if (GUI.Button(elementRect, columns[x][y].texture))
                         {
-                            PuzzleElement tmpElement = columns[x][y];
-                            columns[x][y] = columns[selectedColumn][selectedRow];
-                            columns[selectedColumn][selectedRow] = tmpElement;
-                            selectedColumn = -1;
-                            selectedRow = -1;
-                            StopCoroutine(DetectCombos());
-                            StartCoroutine(DetectCombos());
+                            SwapElements(x, y);
                         }
                     }
                     else
                     {
-                        if (elementRect.Contains(Event.current.mousePosition))
+                        if (elementRect.Contains(Event.current.mousePosition) && Input.GetMouseButtonDown(0))
                         {
-                            GUI.enabled = false;
-                            if (Input.GetMouseButtonDown(0))
-                            {
-                                selectedColumn = x;
-                                selectedRow = y;
-                            }
-                        }
-                        if (x == selectedColumn && y == selectedRow)
-                        {
-                            GUI.enabled = false;
+                            selectedColumn = x;
+                            selectedRow = y;
                         }
                         GUI.Box(elementRect, columns[x][y].texture);
                     }
-
-                    GUI.enabled = true;
                 }
             }
         }
     }
 
+    void SwapElements(int x, int y)
+    {
+        PuzzleElement temp = columns[x][y];
+        columns[x][y] = columns[selectedColumn][selectedRow];
+        columns[selectedColumn][selectedRow] = temp;
+        selectedColumn = -1;
+        selectedRow = -1;
+        StartCoroutine(DetectCombos());
+    }
+
     IEnumerator CompressElements()
     {
-        bool compressionNeeded = false;
-        for (int x = 0; x < columns.Count; x++)
+        yield return new WaitForSeconds(0.25f);
+        foreach (var column in columns)
         {
-            for (int y = 1; y < columns[x].Count; y++)
+            for (int y = column.Count - 1; y >= 0; y--)
             {
-                if(!columns[x][y].texture && columns[x][y - 1].texture)
+                if (column[y].texture == null)
                 {
-                    compressionNeeded = true;
-                }
-            }
-        }
-
-        if (compressionNeeded)
-        {
-            yield return new WaitForSeconds(0.25f);
-
-            for (int x = 0; x < columns.Count; x++)
-            {
-                int referenceIndex = -1;
-                for (int y = columns[x].Count - 1; y >= 0; y--)
-                {
-                    if (!columns[x][y].texture)
+                    for (int above = y - 1; above >= 0; above--)
                     {
-                        if (referenceIndex == -1)
+                        if (column[above].texture != null)
                         {
-                            referenceIndex = y;
-                        }
-                    }
-                    else
-                    {
-                        if (referenceIndex != -1)
-                        {
-                            columns[x][referenceIndex].texture = columns[x][y].texture;
-                            columns[x][referenceIndex].position = columns[x][y].position;
-                            columns[x][y].texture = null;
-                            referenceIndex--;
+                            column[y].texture = column[above].texture;
+                            column[above].texture = null;
+                            break;
                         }
                     }
                 }
             }
         }
-
         StartCoroutine(RestockEnumrator());
     }
 
     IEnumerator RestockEnumrator()
     {
         yield return new WaitForSeconds(0.25f);
-
-        for (int x = 0; x < columns.Count; x++)
+        foreach (var column in columns)
         {
-            for (int y = 0; y < columns[x].Count; y++)
+            for (int y = 0; y < column.Count; y++)
             {
-                if (!columns[x][y].texture)
+                if (column[y].texture == null)
                 {
-                    int randomElement = Random.Range(0, (elements.Length - 1) * 2);
-                    if (randomElement > elements.Length - 1)
-                    {
-                        randomElement -= elements.Length - 1;
-                    }
-                    PuzzleElement element = new PuzzleElement();
-                    element.texture = elements[randomElement];
-                    element.position = new Vector2((Screen.width / 2 - (totalColumns * 64) / 2) + x * 64, (-Screen.height - (totalRows * 64) / 2) + y * 64);
-                    columns[x][y] = element;
+                    column[y].texture = elements[Random.Range(0, elements.Length)];
                 }
             }
         }
@@ -177,104 +143,55 @@ public class PuzzleGenerator : MonoBehaviour
 
     IEnumerator DetectCombos()
     {
-        Debug.Log("DetectCombos() почався!");
-        Debug.Log("DetectCombos() закінчився!");
-        Debug.Log("Очки додано Новий рахунок:" + score);
         if (isCheckingCombos) yield break;
-            isCheckingCombos = true;
-
+        isCheckingCombos = true;
         yield return new WaitForSeconds(0.25f);
-        List<List<int>> combinedLines = new List<List<int>>();
+        
         bool combosDetected = false;
-
-        for (int x = 0; x < columns.Count; x++)
+        HashSet<Vector2Int> toRemove = new HashSet<Vector2Int>();
+        
+        for (int x = 0; x < totalColumns; x++)
         {
-            combinedLines.Add(new List<int>());
-            List<int> line = new List<int>();
-            for (int y = 0; y < columns[x].Count; y++)
+            for (int y = 0; y < totalRows - 2; y++)
             {
-                if(line.Count == 0)
+                if (columns[x][y].texture != null &&
+                    columns[x][y].texture == columns[x][y + 1].texture &&
+                    columns[x][y].texture == columns[x][y + 2].texture)
                 {
-                    line.Add(y);
-                }
-                else
-                {
-                    if(columns[x][line[0]].texture == columns[x][y].texture)
-                    {
-                        line.Add(y);
-                    }
-                    if (columns[x][line[0]].texture != columns[x][y].texture || y == columns[x].Count - 1)
-                    {
-                        if(line.Count >= 3)
-                        {
-                            combinedLines[x].AddRange(line);
-                        }
-                        line.Clear();
-                        line.Add(y);
-                    }
+                    toRemove.Add(new Vector2Int(x, y));
+                    toRemove.Add(new Vector2Int(x, y + 1));
+                    toRemove.Add(new Vector2Int(x, y + 2));
                 }
             }
         }
-
-        for (int x = 0; x < combinedLines.Count; x++)
+        
+        for (int y = 0; y < totalRows; y++)
         {
-            for (int y = 0; y < combinedLines[x].Count; y++)
+            for (int x = 0; x < totalColumns - 2; x++)
             {
-                columns[x][combinedLines[x][y]].texture = null;
-                score += 1;
-                sliderValue += 5f;
-                sliderValue = Mathf.Clamp(sliderValue, 0, 100);
-                scoreSlider.value = sliderValue;
-                combosDetected = true;
-            }
-        }
-        combinedLines = new List<List<int>>();
-        for (int y = 0; y < columns[0].Count; y++)
-        {
-            combinedLines.Add(new List<int>());
-            List<int> line = new List<int>();
-            for (int x = 0; x < columns.Count; x++)
-            {
-                if (line.Count == 0)
+                if (columns[x][y].texture != null &&
+                    columns[x][y].texture == columns[x + 1][y].texture &&
+                    columns[x][y].texture == columns[x + 2][y].texture)
                 {
-                    line.Add(x);
-                }
-                else
-                {
-                    if (columns[line[0]][y].texture == columns[x][y].texture)
-                    {
-                        line.Add(x);
-                    }
-                    if (columns[line[0]][y].texture != columns[x][y].texture || x == columns.Count - 1)
-                    {
-                        if (line.Count >= 3)
-                        {
-                            combinedLines[y].AddRange(line);
-                        }
-                        line.Clear();
-                        line.Add(x);
-                    }
+                    toRemove.Add(new Vector2Int(x, y));
+                    toRemove.Add(new Vector2Int(x + 1, y));
+                    toRemove.Add(new Vector2Int(x + 2, y));
                 }
             }
         }
-
-        for (int x = 0; x < combinedLines.Count; x++)
+        
+        foreach (var pos in toRemove)
         {
-            for (int y = 0; y < combinedLines[x].Count; y++)
-            {
-                columns[combinedLines[x][y]][x].texture = null;
-                score += 1;
-                sliderValue += 5f;
-                sliderValue = Mathf.Clamp(sliderValue, 0, 100);
-                scoreSlider.value = sliderValue;
-                combosDetected = true;
-            }
+            columns[pos.x][pos.y].texture = null;
+            sliderValue = Mathf.Clamp(sliderValue + 5f, 0, 100);
         }
-
-        if (combosDetected)
+        
+        if (toRemove.Count > 0)
         {
             StartCoroutine(CompressElements());
+            combosDetected = true;
         }
+        
         isCheckingCombos = false;
     }
 }
