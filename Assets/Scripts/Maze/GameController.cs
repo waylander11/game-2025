e834c8414922a0559bd7646df5fa904102450c0e
Mr-Fikser <136245570+Mr-Fikser@users.noreply.github.com>
2025-04-03 22:30:03 +0300

    Завершення роботи над Maze та створено острів для шутера

--- Assets/Scripts/Maze/GameController.cs
+++ Assets/Scripts/Maze/GameController.cs
@@ -1,33 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] float Speed = 5f;
+    [SerializeField] GameObject pausePanel;
+    private bool isPaused = false;
     private Rigidbody2D rb;
-    private Animator animator;
     private float moveX;
     private float moveY;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         moveX = Input.GetAxisRaw("Horizontal"); 
         moveY = Input.GetAxisRaw("Vertical");
-
-        animator.SetFloat("moveX", moveX);
-        animator.SetFloat("moveY", moveY);
-        animator.SetFloat("Speed", Mathf.Abs(moveX) + Mathf.Abs(moveY));
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(moveX * Speed, moveY * Speed);
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void GoToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("IslandStart");
     }
 }
---
+++ Assets/Scripts/Maze/WinTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject winPanel;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            winPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
